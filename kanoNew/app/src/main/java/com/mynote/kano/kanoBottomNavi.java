57b6067e66d9793bb1785f52040732cb509a2ae5
jihye7788 <43394982+jihye7788@users.noreply.github.com>
2020-08-21 21:57:15 +0900

    일단커밋

---
+++ kanoNew/app/src/main/java/com/mynote/kano/kanoBottomNavi.java
@@ -0,0 +1,13 @@
+package com.mynote.kano;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class kanoBottomNavi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kano_bottom_navi);
+    }
+}
